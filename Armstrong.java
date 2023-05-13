a3b2e979a1e3f05d71e4c410d53704cfb1b0f09f
TARUNKASHYAP03 <120279744+TARUNKASHYAP03@users.noreply.github.com>
2023-05-13 23:27:04 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner x=new Scanner(System.in);
+        System.out.println("Enter the number = ");
+        int n=x.nextInt();
+        int temp=n;
+        int sum=0,digits=0;
+        while(temp>0){
+            temp/=10;
+            digits++;
+        }
+        temp=n;
+        while(temp>0){
+            int digit =temp%10;
+            sum+=(Math.pow(digit,digits));
+            temp/=10;
+        }
+        if(n==sum){
+            System.out.print(" is a armstrong number");
+        }
+        else{
+            System.out.print(" is not armstron number");
+        }
+        x.close();
+    }
+    
+}
