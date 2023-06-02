02b53219cac99ab18725a1db2048db3a10263cf3
TARUNKASHYAP03 <120279744+TARUNKASHYAP03@users.noreply.github.com>
2023-06-02 20:50:46 +0530

    Add files via upload

--- Hello100t.java
+++ Hello100t.java
@@ -1,9 +1,15 @@
-package Program;
+import java.util.Scanner;
 
 public class Hello100t {
     public static void main(String[] args) {
-        for(int i=0;i<=10;i++){
-            System.out.print("Hello World ");
+        Scanner x=new Scanner(System.in);
+        System.out.println("Enter the your name =");
+        String name=x.nextLine();
+        System.out.println("Enter the number how many time print your name =");
+        int n=x.nextInt();
+        for(int i=0;i<=n;i++){
+        System.out.print(name+ " ");
         }
+        x.close();
         }
     }
