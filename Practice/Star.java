b79026c0455d0c7111da36acd8bfb95cac6294ac
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2023-10-20 09:17:58 +0530

    Add files via upload

---
+++ Practice/Star.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Star {
+    public static void main(String[] args) {
+        Scanner x=new Scanner(System.in);
+        System.out.print("Enter the value = ");
+        int n=x.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        x.close();
+    }
+}
