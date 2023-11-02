19d8d87d372f6b7d1f7d878adf9500f1fdddb654
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2023-11-02 20:08:34 +0530

    Add files via upload

---
+++ FunctionQ/PatternQ/HollowRombus.java
@@ -0,0 +1,30 @@
+package RD.PatternQ;
+
+import java.util.Scanner;
+
+public class HollowRombus {
+
+    public static void print(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || j == 1 || i == n || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size value :");
+        int n = sc.nextInt();
+        print(n);
+        sc.close();
+    }
+}
