d6026dd8edbbef94e4ff8d746b0826f434dc89ab
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2023-11-04 21:55:45 +0530

    Add files via upload

---
+++ FunctionQ/C/Calculator1.java
@@ -0,0 +1,78 @@
+package RD.Function;
+
+import java.util.Scanner;
+
+public class Calculator1 {
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static double divide(double a, double b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            System.out.println("Division by zero is not allowed.");
+            return Double.NaN; // Return a special value for error
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Menu:");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice (1-5): ");
+
+            int choice = scanner.nextInt();
+
+            if (choice == 5) {
+                System.out.println("Exiting the program.");
+                break;
+            }
+
+            if (choice < 1 || choice > 4) {
+                System.out.println("Invalid choice. Please enter a number from 1 to 5.");
+                continue;
+            }
+
+            System.out.print("Enter the first number: ");
+            double num1 = scanner.nextDouble();
+            System.out.print("Enter the second number: ");
+            double num2 = scanner.nextDouble();
+
+            double result = 0;
+
+            switch (choice) {
+                case 1:
+                    result = add(num1, num2);
+                    break;
+                case 2:
+                    result = subtract(num1, num2);
+                    break;
+                case 3:
+                    result = multiply(num1, num2);
+                    break;
+                case 4:
+                    result = divide(num1, num2);
+                    break;
+            }
+
+            System.out.println("Result: " + result);
+        }
+
+        scanner.close();
+    }
+}
