5951df181aa38ff82b167f9966575149f0be8915
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2023-11-04 21:57:06 +0530

    Add files via upload

---
+++ FunctionQ/C/Prime.java
@@ -0,0 +1,42 @@
+package RD.Function;
+
+import java.util.Scanner;
+
+public class Prime {
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        if (number <= 3) {
+            return true;
+        }
+
+        if (number % 2 == 0 || number % 3 == 0) {
+            return false;
+        }
+
+        for (int i = 5; i * i <= number; i += 6) {
+            if (number % i == 0 || number % (i + 2) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        if (isPrime(num)) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+
+        scanner.close();
+    }
+}
