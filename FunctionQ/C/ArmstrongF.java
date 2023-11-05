e373e2f697848432f9093773b7145e39ab76fdc3
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2023-11-05 21:04:03 +0530

    Add files via upload

---
+++ FunctionQ/C/ArmstrongF.java
@@ -0,0 +1,11 @@
+package RD.Function;
+
+import java.util.Scanner;
+
+public class ArmstrongF {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        sc.close();
+    }
+}
