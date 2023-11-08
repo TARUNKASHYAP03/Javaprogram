909e60acde89721435dd0d4eb933aeb76c9380eb
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2023-11-08 10:18:24 +0530

    Add files via upload

---
+++ Java100/AreaTriangle.java
@@ -0,0 +1,10 @@
+package Java100;
+
+import java.util.Scanner;
+
+public class AreaTriangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        sc.close();
+    }
+}
