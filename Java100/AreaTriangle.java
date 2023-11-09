dce9aa91a57bcbde70d9a5080a8edf9a6f706e40
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2023-11-09 13:08:26 +0530

    Add files via upload

--- Java100/AreaTriangle.java
+++ Java100/AreaTriangle.java
@@ -3,8 +3,20 @@ package Java100;
 import java.util.Scanner;
 
 public class AreaTriangle {
+    public static double CalulateArea(int b, int h) {
+        double area = (b * h) / 2;
+        return area;
+    }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the base of triangle :");
+        int b = sc.nextInt();
+        System.out.print("Enter the height of triangle :");
+        int h = sc.nextInt();
+
+        double area = CalulateArea(b, h);
+        System.out.println("Area of triangle " + area);
         sc.close();
     }
 }
