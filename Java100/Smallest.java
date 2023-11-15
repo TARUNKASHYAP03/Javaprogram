0bc6eb65eab86b749d941caa35019a0e5a57f19b
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2023-11-15 18:24:49 +0530

    Update Smallest.java

--- Java100/Smallest.java
+++ Java100/Smallest.java
@@ -1,3 +1,22 @@
+package Java100;
 
+import java.util.Scanner;
+
+public class SmallestNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+      
+        int smallest = (num1 < num2) ? num1 : num2;
+      
+        System.out.println("The smallest number is: " + smallest);
+      
+        scanner.close();
+    }
+}
 
 
