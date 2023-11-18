e4d706a08f18a158fe60343b38eb248d53c17f76
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2023-11-18 21:50:23 +0530

    Add files via upload

---
+++ Java100/SmallestNumber.java
@@ -0,0 +1,24 @@
+package Java100;
+
+import java.util.Scanner;
+
+public class SmallestNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int num3 = sc.nextInt();
+
+        int smallest = (num1 < num2) ? ((num1 < num3) ? num1 : num3) : ((num2 < num3) ? num2 : num3);
+
+        System.out.println("The smallest number among " + num1 + ", " + num2 + ", and " + num3 + " is: " + smallest);
+
+        sc.close();
+    }
+}
