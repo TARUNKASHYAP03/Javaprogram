534e3a9f7e64bfcd06e69898c159dbffa6399399
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2023-11-22 21:34:49 +0530

    Update Condition.java

--- Java100/Condition.java
+++ Java100/Condition.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
+
+public class ConditionalExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input a number from the user
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is positive, negative, or zero
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+        // Check if the number is even or odd
+        if (number % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+
+        // Close the scanner to prevent resource leak
+        scanner.close();
+    }
+}
 
