b573db3aef68b67f03e02a108cd78a15a8e667ff
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2023-12-12 10:45:26 +0530

    Add files via upload

---
+++ Java100/Pattern/Square.java
@@ -0,0 +1,24 @@
+package Java100.Pattern;
+
+import java.util.Scanner;
+
+public class Square {
+
+    public static void patter(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of pattern : ");
+        int n = sc.nextInt();
+        patter(n);
+
+        sc.close();
+    }
+}
