9ebc1bd940712b6a0a31871a7e71727abf606620
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2023-12-12 10:44:44 +0530

    Update Fibonacci.java

--- Java100/for/Fibonacci.java
+++ Java100/for/Fibonacci.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input the number of terms
+        System.out.print("Enter the number of terms for Fibonacci series: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+
+        // Display the Fibonacci series
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+
+        scanner.close();
+    }
+
+    // Recursive method to calculate Fibonacci number
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
 
