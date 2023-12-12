5d256e347ecec251051b0b0cb4668a8a3e1bb5f4
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2023-12-12 10:53:12 +0530

    Add files via upload

---
+++ Java100/Pattern/RightNum.java
@@ -0,0 +1,26 @@
+package Java100.Pattern;
+
+import java.util.Scanner;
+
+public class RightNum {
+    public static int triangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of pattern : ");
+        int n = sc.nextInt();
+        triangle(n);
+        sc.close();
+    }
+}
