3177732081b6c06bde0f07204bb2866af901a560
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2023-12-17 01:06:09 +0530

    Add files via upload

---
+++ Array/DSASHEET/Buy$sell.java
@@ -0,0 +1,24 @@
+package JavaProgram.Array.DSASheet;
+
+public class Buy$shell {
+    public static void stock(int dayprice[]) {
+        int buyP = Integer.MAX_VALUE;
+        int maxprofit = 0;
+
+        for (int i = 0; i < dayprice.length; i++) {
+            if (buyP < dayprice[i]) {
+                int profit = dayprice[i] - buyP;
+                maxprofit = Math.max(maxprofit, profit);
+            } else {
+                buyP = dayprice[i];
+            }
+        }
+        System.out.println("Maxmimum profit : " + maxprofit);
+    }
+
+    public static void main(String[] args) {
+        int dayprice[] = { 7, 1, 5, 3, 6, 4 };
+
+        stock(dayprice);
+    }
+}
