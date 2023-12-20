8d478db228f2896811d66676c484cb262f2e740e
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2023-12-20 18:55:03 +0530

    Add files via upload

---
+++ Array/Sorting/Bubble.java
@@ -0,0 +1,28 @@
+package JavaProgram.Array.Sorting;
+
+public class Bubble {
+    public static void sort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printarr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 4, 2, 6, 3, 7, 4 };
+        sort(arr);
+        printarr(arr);
+    }
+}
