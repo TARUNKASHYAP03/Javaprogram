c6418ce1d21cc5fbbfb181047f2775df31517086
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2024-01-05 00:44:17 +0530

    Add files via upload

---
+++ Array/Sorting/Counting.java
@@ -0,0 +1,40 @@
+package JavaProgram.Array.Sorting;
+
+public class Counting {
+    public static void sorting(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+        int count[] = new int[largest + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        int j = 0;
+
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    public static void printarr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 4, 6, 1, 4, 7, 2, 5, 3, 7, 5, 6 };
+
+        sorting(arr);
+        printarr(arr);
+    }
+}
