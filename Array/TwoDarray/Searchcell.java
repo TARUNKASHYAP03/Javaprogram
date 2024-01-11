5a3d70cc16bfff16d8298de285c3758266196d22
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2024-01-11 11:33:07 +0530

    Update Searchcell.java

--- Array/TwoDarray/Searchcell.java
+++ Array/TwoDarray/Searchcell.java
@@ -1,1 +1,41 @@
+package JavaProgram.Array.TwoDArray;
 
+import java.util.Scanner;
+
+public class Searchcell {
+    static void search(int matrix[][], int key) {
+        int n = matrix.length, m = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (key == matrix[i][j]) {
+                    System.out.println("key is exist at (" + i + "," + j + ")" + "cell");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int matrix[][] = new int[3][3];
+
+        int n = matrix.length, m = matrix.length;
+        System.out.println("Enter the number : ");
+        // Input
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        // Output
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.print("Enter the key : ");
+        int key = sc.nextInt();
+        search(matrix, key);
+        sc.close();
+    }
+}
