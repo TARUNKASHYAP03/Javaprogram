f874dc3b85f483e796cfd1481d3f0af300ff0082
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2024-01-11 10:47:10 +0530

    Update Descounting.java

--- Array/Assignment/Descounting.java
+++ Array/Assignment/Descounting.java
@@ -1,1 +1,41 @@
+package JavaProgram.Array.Assignment;
+
+public class Descounting {
+    public static void sorting(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+        int count[] = new int[largest + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        int j = 0;
+
+        for (int i = count.length - 1; i >= 0; i--) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    public static void printarr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 4, 6, 1, 4, 7, 2, 5, 3, 7, 5, 6 };
+
+        sorting(arr);
+        printarr(arr);
+    }
+}
 
