f05e0bf14f69f1532c456f7dec9aa582cbb3cf9a
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2024-02-21 23:09:10 +0530

    Add files via upload

---
+++ String/Check.java
@@ -0,0 +1,8 @@
+package JavaProgram.String;
+
+public class Check {
+    public static void main(String[] args) {
+        String str = "Tarun".replace("a", "");
+        System.out.println(str);
+    }
+}
