0655cc05b06b6caa56bbcfff3664f7384ac8aedc
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2024-02-29 22:15:46 +0530

    Add files via upload

---
+++ Bit/Countsetbit.java
@@ -0,0 +1,24 @@
+package JavaProgram.Bit;
+
+import java.util.Scanner;
+
+public class Countsetbit {
+    public static int toCountSetBit(int n) {
+        int count = 0;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+        System.out.println(toCountSetBit(n));
+        sc.close();
+    }
+}
