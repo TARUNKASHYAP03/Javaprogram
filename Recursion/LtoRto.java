6d38f7e71b7a00c8a6421e5fb8ff1f3a3a0f096a
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2024-03-11 23:18:25 +0530

    Update LtoRto.java

--- Recursion/LtoRto.java
+++ Recursion/LtoRto.java
@@ -1,1 +1,26 @@
+package JavaProgram.Recursion;
 
+import java.util.Scanner;
+
+public class LtoRto {
+    public static void RTL(int n) {
+        if (n < 0) {
+            System.out.print("-");
+            n = Math.abs(n);
+        }
+
+        while (n > 0) {
+            int digit = n % 10;
+            System.out.println(digit);
+            n /= 10;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number :");
+        int n = sc.nextInt();
+        RTL(n);
+        sc.close();
+    }
+}
