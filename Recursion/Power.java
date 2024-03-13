c3fbbc219ef86c7d293e1f394b1ca52c52549771
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2024-03-13 20:34:59 +0530

    Add files via upload

--- Recursion/Power.java
+++ Recursion/Power.java
@@ -1,21 +1,33 @@
-package JavaProgram.Class;
+package JavaProgram.Recursion;
 
 import java.util.Scanner;
 
 public class Power {
-    static int calcPower(int num, int pow) {
-        if (pow == 1)
-            return num;
-        return num * calcPower(num, pow - 1);
+    static int calPowOfXn(int n, int pow) {
+        if (pow == 0) {
+            return 1;
+        }
+
+        int halfpower = calPowOfXn(n, pow / 2);
+
+        int halfPowersq = halfpower * halfpower;
+
+        // power is odd
+
+        if (pow % 2 != 0) {
+            halfPowersq = n * halfPowersq;
+        }
+
+        return halfPowersq;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the numbers : ");
-        int num = sc.nextInt();
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+        System.out.print("Enter the value of power : ");
         int pow = sc.nextInt();
-
-        System.out.println(num + " power is " + pow + " then result : " + calcPower(num, pow));
+        System.out.println(n + " power " + pow + " : " + calPowOfXn(n, pow));
         sc.close();
     }
 }
