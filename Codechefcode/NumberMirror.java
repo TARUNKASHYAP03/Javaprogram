1e54374b064f06b00a8ba410f63928d9207cd12a
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2024-04-17 12:57:54 +0530

    Add files via upload

---
+++ Codechefcode/NumberMirror.java
@@ -0,0 +1,18 @@
+package JavaProgram.Codechef;
+
+import java.util.Scanner;
+
+public class NumberMirror {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+
+        int number = sc.nextInt();
+
+        System.out.println("The number you entered is: " + number);
+
+        sc.close();
+    }
+}
