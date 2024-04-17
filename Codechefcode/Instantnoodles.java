1f11eec67545f962acc0482213544d099393a985
Tarun Kumar <120279744+TARUNKASHYAP03@users.noreply.github.com>
2024-04-17 12:58:15 +0530

    Add files via upload

---
+++ Codechefcode/Instantnoodles.java
@@ -0,0 +1,16 @@
+package JavaProgram.Codechef;
+
+import java.util.*;
+
+class Instantnoodles
+{
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Output : " + (a * b));
+        sc.close();
+
+    }
+}
